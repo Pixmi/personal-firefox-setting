2d493d0bb2fda3fc3e6153f48a09249ffd0a750c
Pixmi <admin@pixmi.dev>
2024-11-22 14:11:00 +0800

    add wenku8 style

---
+++ userscript/wenku8.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        [UserStyle] Wenku8
+// @namespace   https://github.com/Pixmi/
+// @version     1.0.0
+// @description for wenku8.net
+// @author      Pixmi
+// @license     GPL-3.0
+// @homepage    https://github.com/Pixmi/personal-firefox-setting
+// @downloadURL https://github.com/Pixmi/personal-firefox-setting/raw/main/userscript/wenku8.user.js
+// @icon        https://github.com/Pixmi/personal-firefox-setting/raw/main/icons/css.svg
+// @match       *://*.wenku8.net/*
+// @resource    UserStyle https://github.com/Pixmi/personal-firefox-setting/raw/main/userstyle/wenku8.user.css
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// @noframes
+// @compatible  Chrome
+// @compatible  Firefox
+// ==/UserScript==
+
+GM_addStyle(GM_getResourceText('UserStyle'));
