0de1a19fdef7bfff430059d43a8587ae8ff7ffcb
Pixmi <admin@pixmi.dev>
2024-11-22 13:14:54 +0800

    add gamer.com.tw style

---
+++ userscript/baha-small-window-style.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        BAHA small window style
+// @namespace   https://github.com/Pixmi/
+// @version     1.0.0
+// @description for PC side by small window
+// @author      Pixmi
+// @license     GPL-3.0
+// @homepage    https://github.com/Pixmi/personal-firefox-setting
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=gamer.com.tw
+// @match       *://*.gamer.com.tw/*
+// @resource    CustomCss https://github.com/Pixmi/personal-firefox-setting/userstyle/baha-small-window-style.css
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// @noframes
+// @compatible  Chrome
+// @compatible  Firefox
+// ==/UserScript==
+
+GM_addStyle(GM_getResourceText('CustomCss'));
